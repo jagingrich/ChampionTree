89bfb15949048c027c0bcafefdac39dfacf6ad63
jagingrich <jared.gingrich@gmail.com>
2024-05-23 14:17:36 -0400

    Initial upload
    Initial file upload, including Potree packaging of Centennial Bur Oak LiDAR point cloud

---
+++ pointclouds/CentennialBurOak/cloud.js
@@ -0,0 +1,26 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 134194466,
+    "boundingBox": {
+        "lx": 441571.875,
+        "ly": 316556.063,
+        "lz": 189.346,
+        "ux": 441730.593,
+        "uy": 316714.781,
+        "uz": 348.0639999999935
+    },
+    "tightBoundingBox": {
+        "lx": 441571.875,
+        "ly": 316556.063,
+        "lz": 189.346,
+        "ux": 441719.375,
+        "uy": 316714.781,
+        "uz": 218.993
+    },
+    "pointAttributes": "LAZ",
+    "spacing": 1.3745381832122803,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
